f0e75b66484ca5bea496e98121f4e6ee96375fb0
Shimol-Khan <se.shimolkhan@gmail.com>
2019-11-21 13:28:38 +0800

    Integrated SendGrid To Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,14 +98,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'https://devise-action-mailer.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'], 
-    domain: "smtp.gmail.com",
-    openssl_verify_mode: "none",
+  
+  ActionMailer::Base.smtp_settings = {
+   address: "smtp.sendgrid.net",
+   port: 25,
+   domain: "heroku.com",
+   user_name: ENV["SENDGRID_USERNAME"],
+   password: ENV["SENDGRID_PASSWORD"],
+   authentication: "plain",
+   enable_starttls_auto: true
   }
+
 end
